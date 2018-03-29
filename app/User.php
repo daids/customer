0b559171b6535d23683e4ba16d257d047bc3b1a5
daidongsheng <daids@mail.com>
2018-03-29 10:07:47 +0800

    update

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'token'
+        'name', 'email', 'password', 'active_token', 'reset_token'
     ];
 
     /**
