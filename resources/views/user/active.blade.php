e997ba1aac1b636538e3b0de6f1b1a5ed61b8243
daidongsheng <daids@mail.com>
2018-05-14 16:08:17 +0800

    update

---
+++ resources/views/user/active.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head><meta name="robots" content="noindex,nofollow,noarchive" />
+<title>Drevo.net</title>
+<body bgcolor="#FFFFFF">
+<center><h1>账号激活</h1></center>
+@if ($resut)
+<p>账号已激活，请登录！</p>
+@else
+<p>账号激活失败，请重新激活！</p>
+@endif
+</body>
+</html>
