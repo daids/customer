87f5af5a6192b69bdc9b972ae367e4b912c319f9
daidongsheng <daids@mail.com>
2018-05-21 16:08:22 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,18 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
-$app->post('/info', 'InfoController@add');
-$app->get('/active/{code}', 'UserController@active');
-$app->post('/user/create', 'UserController@create');
-$app->post('/user/resetpassword', 'UserController@resetPassword');
-$app->post('/user/getresetpasswordtoken', 'UserController@getResetPasswordToken');
-$app->post('/user/login', 'UserController@login');
-
-$app->post('/user/file', 'UserController@uploadFile');
-$app->get('/user/file/{name}', 'UserController@file');
-
-$app->post('/software/feedback', 'UserController@feedback');
-
 $app->group(['middleware' => 'api.log'], function () use ($app) {
-    $app->get('/test', 'UserController@test');
+
+    $app->post('/info', 'InfoController@add');
+
+    $app->get('/active/{code}', 'UserController@active');
+    $app->post('/user/create', 'UserController@create');
+    $app->post('/user/resetpassword', 'UserController@resetPassword');
+    $app->post('/user/getresetpasswordtoken', 'UserController@getResetPasswordToken');
+    $app->post('/user/login', 'UserController@login');
+
+    $app->post('/user/file', 'UserController@uploadFile');
+    $app->get('/user/file/{name}', 'UserController@file');
+    
+    $app->post('/software/feedback', 'UserController@feedback');
 });
